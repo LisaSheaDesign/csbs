aa5949bb8c21a46d006f4162baa2b822aedbc3e5
LisaSheaDesign <LisaSheaDesign@gmail.com>
2016-08-28 18:36:49 -0700

    add handlebar code

--- gulpfile.js
+++ gulpfile.js
@@ -7,17 +7,30 @@ var rename = require('gulp-rename');
 var cssmin = require('gulp-cssmin');
 var clean = require('gulp-clean');
 var concat = require('gulp-concat');
+var hb = require('gulp-hb');
+
+//handlebars
+gulp.task('hb', function () {
+    return gulp
+        .src('./src/{,posts/}*.html')
+        .pipe(hb({
+            partials: './src/assets/partials/**/*.hbs',
+            helpers: './src/assets/helpers/*.js',
+            data: './src/assets/data/**/*.{js,json}'
+        }))
+        .pipe(gulp.dest('./web'));
+});
 
 //clean out min dir
 gulp.task('clean-css', function() {
   return gulp.src('_css/min/main.min.css')
   .pipe(clean());
-})
+});
 
 gulp.task('clean-js', function() {
   return gulp.src('_js/min/script.min.js')
   .pipe(clean());
-})
+});
 
 // uglify js
 gulp.task('jsmin', ['clean-js'], function() {
@@ -37,12 +50,12 @@ gulp.task('cssmin', ['clean-css'], function(){
     suffix: '.min'
   }))
   .pipe(gulp.dest('_css/min'));
-})
+});
 
 //watch js and css files
 gulp.task('watch', function() {
   gulp.watch('_css/main.css', ['cssmin']);
   gulp.watch('_js/script.js', ['jsmin']);
-})
+});
 
 gulp.task('default',['jsmin', 'cssmin', 'watch']);
