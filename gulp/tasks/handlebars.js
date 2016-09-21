7973babab3a870861537873ddbcea5fc3f5488c1
LisaSheaDesign <LisaSheaDesign@gmail.com>
2016-09-20 18:47:54 -0700

    create src dir, made temp pages

--- gulp/tasks/handlebars.js
+++ gulp/tasks/handlebars.js
@@ -4,34 +4,55 @@ var gulp = require('gulp'),
 
 //handlebars
 gulp.task('handlebars', function() {
-  var event = {"show":
+var event = {
+
+  "evt1":
     {
-      "name": "Rock Festival",
-      "band": "GNR"
+      "title": "4th of July - closed Monday July 4th",
+      "text": "Please make appointments in advance."
+    },
+  "evt2":
+    {
+      "title": "Road Construction on Whitsett Ave",
+      "text": 'Prepare to arrive a little earlier than normal due to road construction on Whitsett Ave, driveway into parking lot is still accessible. <a href="*" data-toggle="modal" data-target="#eModal">See map for details.</a>'
     }
-},
-  options = {
-    batch:['./_includes']
+  },
+    options = {
+    batch:['./src/partials']
   }
 
   var index = function() {
-    gulp.src('./index.hbs')
-    .pipe(handlebars(event.show, options))
+    gulp.src('./src/temps/index.hbs')
+    .pipe(handlebars(event.evt1, options))
     .pipe(rename('index.html'))
     .pipe(gulp.dest('./'));
   }
 
-  var about = function() {
-    gulp.src('about-cstyles-barber-spa/index.hbs')
+  var tempPage = function(src, place) {
+    gulp.src('./src/temps/' + src + '.hbs')
     .pipe(handlebars(options))
     .pipe(rename('index.html'))
-    .pipe(gulp.dest('./about-cstyles-barber-spa'));
+    .pipe(gulp.dest('./' + place));
   }
 
 
 var init = function() {
   index();
-  about();
+  tempPage('about', 'about-cstyles-barber-spa');
+  tempPage('cast', 'cast-cstyles-barber-spa');
+  tempPage('color', 'barber-spa-hair-color');
+  tempPage('contact', 'contact-cstyles-barber-spa');
+  tempPage('cuts', 'barber-spa-hair-cuts');
+  tempPage('events', 'events-cstyles-barber-spa');
+  tempPage('manicures', 'barber-spa-hair-manicures');
+  tempPage('news', 'news-cstyles-barber-spa');
+  tempPage('news-gent', 'news-cstyles-barber-spa/BestGentleman-The-Barber-Review-2012');
+  tempPage('news-sanvalley', 'news-cstyles-barber-spa/San-Fernando-Valley-Business-Journal-2015');
+  tempPage('news-valley', 'news-cstyles-barber-spa/Best-of-Valley-Village-Award-2014');
+  tempPage('packages', 'barber-spa-packages');
+  tempPage('photo', 'photos-cstyles-barber-spa');
+  tempPage('services', 'barber-spa-services');
+  tempPage('shaves', 'barber-spa-shaves');
 }
   return init();
 
